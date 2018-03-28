d0f4e923a2aef1e142b06d8d1eba6d3025c43d28
john <sato.keita@furyu.jp>
2018-03-28 12:03:08 +0900

    一応data bindingした

--- src/index.vue
+++ src/index.vue
@@ -1,5 +1,23 @@
 <template>
     <section>
-        <h2>vue内のコンポーネントに書いているよ！</h2>
+        <p>johnさんの言葉: {{greeting}}</p>
     </section>
 </template>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        greeting: 'こんにちは',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+    color: #2e9afe;
+  }
+</style>
