8de8afe7833d013148dae6e28ea5f0361c78a61e
john <sato.keita@furyu.jp>
2018-03-28 11:29:22 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import index from './index.vue';
+
+new Vue(index).$mount('#app');
---
+++ src/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h2>hogehoge</h2>
+        <div></div>
+    </section>
+</template>
